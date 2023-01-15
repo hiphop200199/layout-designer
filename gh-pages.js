0f56212ea9c323e86fc3ce4377d4809f88b3bb6a
Eric Tsai <hiphop200199@gmail.com>
2023-01-16 02:29:01 +0800

    deploy

--- gh-pages.js
+++ gh-pages.js
@@ -4,10 +4,10 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository  
+        repo: 'https://github.com/hiphop200199/layout-designer.git', // Update to point to your repository  
         user: {
-            name: 'Your name', // update to use your name
-            email: 'Your Email address' // Update to use your email
+            name: 'hiphop200199', // update to use your name
+            email: 'hiphop200199@gmail.com' // Update to use your email
         }
     },
     () => {
